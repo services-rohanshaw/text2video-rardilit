777f103039b535e02a00dc886a4cff93321feb52
services-rohanshaw <132089785+services-rohanshaw@users.noreply.github.com>
2023-05-19 18:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import diffusers
 
 # Load the diffusion pipeline
-pipeline = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b")
+pipeline = diffusers.DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b")
 
 # Create a text input field
 text_input = st.text_input("Enter your text here")
